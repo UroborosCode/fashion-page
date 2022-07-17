08e5c55accd42fd7201261df6da931dbb0c0f87f
UroborosCode <98331306+UroborosCode@users.noreply.github.com>
2022-07-17 13:36:09 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function () {
+
+  // header__nav--media
+
+  $('.header__btn-open').on('click', function (){
+    $('.header__nav').addClass('header__nav--media')
+})
+$('.header__btn-close').on('click', function (){
+    $('.header__nav').removeClass('header__nav--media')
+})
+
+  // slick-slider js
+    // $('.').slick({
+    //     dots: true,
+    //     arrows: true,
+    //     fade: false,
+    //     autoplay: false,
+    //     slidesToScroll: 1,
+    // });
+
+})
